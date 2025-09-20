a2ae8dbc038a467a16b31a82bc075a96736c590d
DanielHristov758 <daniel.p.hristov@gmail.com>
2025-09-20 21:40:27 +0300

    ,

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -38,7 +38,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               87
             </div>
