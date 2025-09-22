c4bf11b27d02bffe5a320c07175ae27fec286b17
DanielHristov758 <daniel.p.hristov@gmail.com>
2025-09-22 15:59:09 +0300

    ,

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ const products = [{
 }, 
 {
      image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotoon T-shirt - 2 pack',
+    name: '2 slot toaster - Black',
     rating: {
         stars: 4.5,
         count: 56
