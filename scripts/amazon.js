68c45a5d83490078e38bf16b28e022d088ee1c23
DanielHristov758 <daniel.p.hristov@gmail.com>
2025-09-23 21:20:34 +0300

    ,

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -60,7 +60,6 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    const productName = button.dataset.productName;
     const productId = button.dataset.productId;
     let matchingItem;
     
